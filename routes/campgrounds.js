d09426defad768006fffd14a25a65d1603e37ac9
Danielle <danielle@kendallspace.net>
2023-04-03 16:24:40 -0400

    restructure app.js to move review routes into their own file

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const catchAsync = require("../utilities/catchAsync");
+
 const { campgroundSchema } = require("../schemas");
+
+const catchAsync = require("../utilities/catchAsync");
 const ExpressError = require("../utilities/ExpressError");
+
 const Campground = require("../models/campground");
 
 const validateCampground = (req, res, next) => {
@@ -31,8 +34,6 @@ router.post(
   "/",
   validateCampground,
   catchAsync(async (req, res, next) => {
-    // if (!req.body.campground)
-    //   throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
