3ebd4bcb51b5a7b0b18db59af03551a74bfdbb75
Danielle <danielle@kendallspace.net>
2023-04-10 18:10:58 -0400

    add author to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -43,6 +43,7 @@ router.post(
   validateCampground,
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Successfully created a new campground!");
     res.redirect(`/campgrounds/${campground._id}`);
@@ -53,9 +54,9 @@ router.post(
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate("reviews")
+      .populate("author");
     if (!campground) {
       req.flash("error", "Campground not found.");
       res.redirect("/campgrounds");
