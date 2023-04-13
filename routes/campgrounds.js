68323231b4c3d7d6830f3d5b819ef5dae06d48c3
Danielle <danielle@kendallspace.net>
2023-04-13 15:27:23 -0400

    add multer middleware for image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utilities/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("It worked!");
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
