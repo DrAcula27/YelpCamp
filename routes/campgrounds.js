f9d251f677c8182d054ad6c52169ca18e9ab024d
Danielle <danielle@kendallspace.net>
2023-04-14 11:02:55 -0400

    setup cloudinary image storage

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,8 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utilities/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 router
   .route("/")
