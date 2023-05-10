5ecb7672a828504522ae95e8c28ff174ae24be4d
Danielle <danielle@kendallspace.net>
2023-05-10 13:13:13 -0400

    add map marker popup

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,3 +1,5 @@
+const campground = require("../../models/campground");
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
@@ -17,4 +19,11 @@ const map = new mapboxgl.Map({
   },
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
