011c8ce294e8631775ab81fb701c7861b9eed444
Danielle <danielle@kendallspace.net>
2023-05-11 14:23:19 -0400

    UI/UX cluster map tweaks

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -11,10 +11,9 @@ map.on("load", () => {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
-  map.addSource("earthquakes", {
+  map.addSource("campgrounds", {
     type: "geojson",
-    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+    // Point to GeoJSON data.
     data: campgrounds,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
@@ -24,7 +23,7 @@ map.on("load", () => {
   map.addLayer({
     id: "clusters",
     type: "circle",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["has", "point_count"],
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -48,7 +47,7 @@ map.on("load", () => {
   map.addLayer({
     id: "cluster-count",
     type: "symbol",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["has", "point_count"],
     layout: {
       "text-field": ["get", "point_count_abbreviated"],
@@ -60,13 +59,13 @@ map.on("load", () => {
   map.addLayer({
     id: "unclustered-point",
     type: "circle",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["!", ["has", "point_count"]],
     paint: {
       "circle-color": "#11b4da",
-      "circle-radius": 4,
+      "circle-radius": 5,
       "circle-stroke-width": 1,
-      "circle-stroke-color": "#fff",
+      "circle-stroke-color": "#84d6ea",
     },
   });
 
@@ -77,7 +76,7 @@ map.on("load", () => {
     });
     const clusterId = features[0].properties.cluster_id;
     map
-      .getSource("earthquakes")
+      .getSource("campgrounds")
       .getClusterExpansionZoom(clusterId, (err, zoom) => {
         if (err) return;
 
@@ -114,6 +113,12 @@ map.on("load", () => {
     map.getCanvas().style.cursor = "pointer";
   });
   map.on("mouseleave", "clusters", () => {
+    map.getCanvas().style.cursor = "";
+  });
+  map.on("mouseenter", "unclustered-point", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
+  map.on("mouseleave", "unclustered-point", () => {
     map.getCanvas().style.cursor = "";
   });
 });
