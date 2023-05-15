3d5c22ccce5a3d677c02f462506a55e5801a8819
Danielle <danielle@kendallspace.net>
2023-05-15 11:38:13 -0400

    add sanitize-html package, modify joi schema to prevent html/script injection into campground fields and reviews

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(flash());
 app.use((req, res, next) => {
-  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
