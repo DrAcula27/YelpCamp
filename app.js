17b5eab6252ae6be7e4530098d2ff7b4b8a05841
Danielle <danielle@kendallspace.net>
2023-05-15 10:54:54 -0400

    add express-mongo-sanitize package

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const LocalStrategy = require("passport-local");
 
 const User = require("./models/user");
 
+const mongoSanitize = require("express-mongo-sanitize");
+
 const ExpressError = require("./utilities/ExpressError");
 
 const userRoutes = require("./routes/users");
@@ -46,6 +48,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: "secret",
@@ -68,6 +71,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(flash());
 app.use((req, res, next) => {
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
